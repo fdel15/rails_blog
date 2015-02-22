b0872f5fcf598a4a4c2867e346eb61ee4497eb73
Frank DelPidio <frankdaedalus@gmail.com>
2015-02-22 12:11:05 -0500

    Controller: posts: create create method

--- blog/app/controllers/posts_controller.rb
+++ blog/app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+  respond_to :html, :json
 
   def index
     @posts = Post.all
@@ -7,5 +8,11 @@ class PostsController < ApplicationController
   def new
     @post = Post.new
   end
+
+  def create
+    @post = Post.new
+    respond_with @post
+  end
+
 
 end
