6912ff6fcab293d9d1564b8db57cded41416a282
Frank DelPidio <frankdaedalus@gmail.com>
2015-02-23 00:08:25 -0500

    Models: create post_date method

--- blog/app/models/post.rb
+++ blog/app/models/post.rb
@@ -2,4 +2,9 @@ class Post < ActiveRecord::Base
   validates :title, presence: true
   validates :body, presence: true
   validates :category_id, presence: true
+
+  def post_date
+    self.created_at.strftime("%B %d, %Y")
+  end
+
 end
