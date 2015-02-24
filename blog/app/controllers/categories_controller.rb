0902ebbdb68b8c7873ecf499544e912b49d6953a
Frank DelPidio <frankdaedalus@gmail.com>
2015-02-24 15:50:33 -0500

    Controllers: create post method for new categories

--- blog/app/controllers/categories_controller.rb
+++ blog/app/controllers/categories_controller.rb
@@ -5,4 +5,20 @@ class CategoriesController < ApplicationController
     @category = Category.new
   end
 
+  def create
+    @category = Category.new(category_params)
+    if @category.save
+      flash[:notice] = "category successfully created!"
+    else
+      flash[:notice] = "Error: category not created"
+    end
+    respond_with(@category, location: new_post_url)
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit!
+  end
+
 end
